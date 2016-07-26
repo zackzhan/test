e386925e36ccbf3f4a72153fd6cf46ef3614a82b
zhanjuzhang <zhanjuzhang@meizu.com>
2016-07-26 15:03:23 +0800

    test composer

---
+++ src/Juzhang/Test/Test.php
@@ -0,0 +1,13 @@
+<?php
+namespace Juzhang\Test;
+
+class Test {
+
+    public function first() {
+        echo 'hello world';
+    }
+
+    public function second() {
+        echo 'second hello';
+    }
+}
---
+++ src/Juzhang/functions.php
@@ -0,0 +1,5 @@
+<?php
+
+function qq() {
+    echo 'qq';
+}
